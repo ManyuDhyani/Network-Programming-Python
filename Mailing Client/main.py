5db6c4a75bfb4d19a9d54e88c0df0c4037a4197c
ManyuDhyani <manyudhyani@gmail.com>
2022-03-19 20:13:40 +0530

    Attach Message to the mail

--- Mailing Client/main.py
+++ Mailing Client/main.py
@@ -1,4 +1,8 @@
 import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
 server = smtplib.SMTP('smtp.gmail.com', 25)#587
 
@@ -13,3 +17,13 @@ with open('pswd.txt', 'r') as f:
     password = f.read()
 
 server.login('manyudhyani@gmail.com', password)
+
+msg = MIMEMultipart()
+msg['From'] = 'Manyu Dhyani'
+msg['To'] = 'manyudhyanipankaj@gmail.com'
+msg['Subject'] = "Just A Test"
+
+with open('message.txt', 'r') as f:
+    message = f.read()
+
+msg.attach(MIMEText(message, 'plain'))
