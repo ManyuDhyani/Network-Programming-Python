75445c1d9e4ff10df2c35b30473c9b3929c70ec9
ManyuDhyani <manyudhyani@gmail.com>
2022-03-19 20:34:05 +0530

    Added Image to the mail and other correction to the code

--- Mailing Client/main.py
+++ Mailing Client/main.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-server = smtplib.SMTP('smtp.gmail.com', 25)#587
+server = smtplib.SMTP('smtp.gmail.com', 25)
 
 """
     Use the EHLO command to identify the domain name of the 
@@ -15,11 +15,15 @@ server.ehlo()
 
 with open('pswd.txt', 'r') as f:
     password = f.read()
-
+#server = smtplib.SMTP('smtp.example.com', 25)
+server.connect("smtp.gmail.com", 587)
+server.ehlo()
+server.starttls()
+server.ehlo()
 server.login('manyudhyani@gmail.com', password)
 
 msg = MIMEMultipart()
-msg['From'] = 'Manyu Dhyani'
+msg['From'] = 'ManyuDhyani'
 msg['To'] = 'manyudhyanipankaj@gmail.com'
 msg['Subject'] = "Just A Test"
 
@@ -27,3 +31,17 @@ with open('message.txt', 'r') as f:
     message = f.read()
 
 msg.attach(MIMEText(message, 'plain'))
+
+filename = '11.JPG'
+attachment = open(filename, 'rb')
+
+p = MIMEBase('application', 'octet-stream')
+p.set_payload(attachment.read())
+
+encoders.encode_base64(p)
+
+p.add_header('Content-Disposition', f'attachment; filename={filename}')
+msg.attach(p)
+
+text = msg.as_string()
+server.sendmail('manyudhyani@gmail.com', 'manyudhyanipankaj@gmail.com', text)
