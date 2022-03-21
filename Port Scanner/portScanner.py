fe83e1ff258c0a385b54bab78e9e2fa083c852be
ManyuDhyani <manyudhyani@gmail.com>
2022-03-21 20:12:22 +0530

    Improved the speed of Port Scanner by using MultiThreading and Queue

--- Port Scanner/portScanner.py
+++ Port Scanner/portScanner.py
@@ -1,7 +1,14 @@
+from concurrent.futures import thread
 import queue
 import socket
+import threading
+from queue import Queue
 
-target = "Some URL or IP Address"
+target = "Some URL or IP address"
+
+
+queue = Queue()
+open_ports = []
 
 def portScan(port):
     try:
@@ -11,11 +18,45 @@ def portScan(port):
     except:
         return False
 
-for port in range(1, 1024):
-    result = portScan(port)
-    if result:
-        print("Port {} is open!".format(port))
-    else:
-        print("Port {} is closed!".format(port))
+# This for loop in slow so we can use Multithreading.
+# Multithreading can be also inefficient as it can check same more than Once.
+# SO will use threading with Queue.
+
+# for port in range(1, 1024):
+#     result = portScan(port)
+#     if result:
+#         print("Port {} is open!".format(port))
+#     else:
+#         print("Port {} is closed!".format(port))
 
 
+def fill_queue(port_list):
+    for port in port_list:
+        queue.put(port)
+
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portScan(port):
+            print("Port {} is open!". format(port))
+
+port_list = range(1, 1024)
+fill_queue(port_list)
+
+thread_list = []
+
+# Define no. of threads
+for t in range(500):
+    thread = threading.Thread(target=worker)
+    thread_list.append(thread)
+
+# To start the Threads in the list
+for thread in thread_list:
+    thread.start()
+
+# We need the Thread List so that we can wait for all the threads to finish
+for thread in thread_list:
+    thread.join()
+    # join() waits until thread is done
+
+print("Open ports are: ", open_ports)
