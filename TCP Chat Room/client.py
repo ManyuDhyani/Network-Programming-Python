8dc3fb339ce790efde3859a2e31b574bc568bd33
ManyuDhyani <manyudhyani@gmail.com>
2022-03-22 21:46:45 +0530

    Client Code: To connect to the server

---
+++ TCP Chat Room/client.py
@@ -0,0 +1,35 @@
+from concurrent.futures import thread
+from email import message
+from http import client
+import socket
+import threading
+from tkinter.tix import Tree
+
+nickname = input("Choose a nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1',  44444))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == "NICKNAME":
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occured!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
